4936d35be5748224bf4598a6a7d5dd0ec2a2722a
Hiroki Chiyoda <contact@arafipro.com>
2026-02-04 20:30:53 +0900

    feat(api): APIエンドポイントの初期実装
    - Honoフレームワークを使用したCRUD操作のためのAPIエンドポイントを実装
    - `@workspace/db` を利用したデータベース操作を統合
    - Todoアイテムの取得、作成、更新、削除機能を提供

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,72 @@
-import { Hono } from "hono";
-import { getDb } from "@workspace/db";
+import { eq, getDb } from "@workspace/db";
 import * as schema from "@workspace/db/schema";
+import { Hono } from "hono";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>().basePath("/api");
 
-app.get("/todos", async (c) => {
-  const db = getDb(c.env);
-  const todos = await db.select().from(schema.todoTable);
-  return c.json(todos);
-});
+type Todo = typeof schema.todoTable.$inferInsert;
+
+app
+  // 1件データ取得
+  .get("/todos/:id", async (c) => {
+    const id = c.req.param("id");
+    try {
+      const db = getDb(c.env);
+      const results = await db
+        .select()
+        .from(schema.todoTable)
+        .where(eq(schema.todoTable.id, id));
+      return c.json(results[0]);
+    } catch (e) {
+      return c.json({ error: e }, 500);
+    }
+  })
+  // 全件データ取得
+  .get("/todos", async (c) => {
+    try {
+      const db = getDb(c.env);
+      const todos = await db.select().from(schema.todoTable);
+      return c.json(todos);
+    } catch (e) {
+      return c.json({ error: e }, 500);
+    }
+  })
+  // 新規登録
+  .post("/todos", async (c) => {
+    const todo = await c.req.json<Todo>();
+    try {
+      const db = getDb(c.env);
+      await db.insert(schema.todoTable).values(todo);
+      return c.json({ message: "Success" }, 201);
+    } catch (e) {
+      return c.json({ error: e }, 500);
+    }
+  })
+  // 更新
+  .put("/todos/:id", async (c) => {
+    const id = c.req.param("id");
+    const todo = await c.req.json<Todo>();
+    try {
+      const db = getDb(c.env);
+      await db
+        .update(schema.todoTable)
+        .set(todo)
+        .where(eq(schema.todoTable.id, id));
+      return c.json({ message: "Success" }, 200);
+    } catch (e) {
+      return c.json({ error: e }, 500);
+    }
+  })
+  // 削除
+  .delete("/todos/:id", async (c) => {
+    const id = c.req.param("id");
+    try {
+      const db = getDb(c.env);
+      await db.delete(schema.todoTable).where(eq(schema.todoTable.id, id));
+      return c.json({ message: "Success" }, 200);
+    } catch (e) {
+      return c.json({ error: e }, 500);
+    }
+  });
 
 export default app;
